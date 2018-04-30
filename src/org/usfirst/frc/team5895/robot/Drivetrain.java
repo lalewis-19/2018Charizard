ecc4b96f1d2665f42a5464befc03a1ef60d3403b
Arthur <lalewis-19@peddie.org>
2018-04-30 16:30:30 -0400

    added electrical layout and drivetrain class

---
+++ src/org/usfirst/frc/team5895/robot/Drivetrain.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team5895.robot;
+
+public class Drivetrain {
+
+}
---
+++ src/org/usfirst/frc/team5895/robot/ElectricalLayout.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team5895.robot;
+
+public class ElectricalLayout {
+	
+	// motors
+	public static final int MOTOR_DRIVE_LEFT_1 = 1;
+	public static final int MOTOR_DRIVE_LEFT_2 = 2;
+	public static final int MOTOR_DRIVE_LEFT_3 = 3;
+	public static final int MOTOR_DRIVE_RIGHT_1 = 4;
+	public static final int MOTOR_DRIVE_RIGHT_2 = 5;
+	public static final int MOTOR_DRIVE_RIGHT_3 = 6;
+	public static final int MOTOR_ARM = 7;
+	
+	// Solenoids
+    public static final int SOLENOID_ARM_LEFT = 8;
+    public static final int SOLENOID_ARM_RIGHT = 9;
+    
+    
+	
+}
