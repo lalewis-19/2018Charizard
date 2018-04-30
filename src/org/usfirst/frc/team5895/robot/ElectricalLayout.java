b2c19b0b22a9315b4482d4a41c637db190a22484
Arthur <lalewis-19@peddie.org>
2018-04-30 16:50:46 -0400

    made changes to electrical layout

--- src/org/usfirst/frc/team5895/robot/ElectricalLayout.java
+++ src/org/usfirst/frc/team5895/robot/ElectricalLayout.java
@@ -3,18 +3,19 @@ package org.usfirst.frc.team5895.robot;
 public class ElectricalLayout {
 	
 	// motors
-	public static final int MOTOR_DRIVE_LEFT_1 = 1;
-	public static final int MOTOR_DRIVE_LEFT_2 = 2;
-	public static final int MOTOR_DRIVE_LEFT_3 = 3;
-	public static final int MOTOR_DRIVE_RIGHT_1 = 4;
-	public static final int MOTOR_DRIVE_RIGHT_2 = 5;
-	public static final int MOTOR_DRIVE_RIGHT_3 = 6;
+	public static final int MOTOR_DRIVE_LEFT_MASTER = 1;
+	public static final int MOTOR_DRIVE_LEFT_FOLLOWER_1 = 2;
+	public static final int MOTOR_DRIVE_LEFT_FOLLOWER_2 = 3;
+	public static final int MOTOR_DRIVE_RIGHT_MASTER = 4;
+	public static final int MOTOR_DRIVE_RIGHT_FOLLOWER_1 = 5;
+	public static final int MOTOR_DRIVE_RIGHT_FOLLOWER_2 = 6;
 	public static final int MOTOR_ARM = 7;
 	
 	// Solenoids
     public static final int SOLENOID_ARM_LEFT = 8;
     public static final int SOLENOID_ARM_RIGHT = 9;
     
-    
+    // Analog Input
+    public static final int SENSOR_ARM_POTENTIOMETER = 1;
 	
 }
